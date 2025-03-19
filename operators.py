3fdf17fd25dc6bb83d5516c36259a84a7347ebdc
Robert Strzelczyk <Robert.Strzelczyk@onexgroup.pl>
2025-03-19 15:56:49 +0100

    add operators task

--- operators.py
+++ operators.py
@@ -25,3 +25,36 @@ if (50 != 20):
 
 if ('hello' == 'goodbye'):
     print("hello jest równe goodbye")
+
+
+# Operatory logiczne
+
+
+age = 12
+height = 150
+
+if (age >= 10 and height >= 140):
+    print("Osoba może przejechać się rollecoasterem")
+
+
+hoursWorked = 9
+projectFinished = True
+
+if (hoursWorked > 8 and projectFinished == True):
+    print("Możesz iść do domu")
+
+temp = 35
+
+if (temp < 0 or temp > 30):
+    print("Ekstremalne warunki pogodowe")
+
+isHoliday = False
+
+if (not(isHoliday)):
+    print("Dziś jest dzień roboczy")
+
+errorsFound = 12
+warnings = 1
+
+if (not(errorsFound < 10 and warnings == 0)):
+    print("Sprawdź kod jeszcze raz")
